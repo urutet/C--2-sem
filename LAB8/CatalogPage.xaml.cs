4c7a21d4bd752affd49754c8b0d66f0a0485bf45
Ilya Yushkevich <ilyshka88@gmail.com>
2021-04-20 00:49:12 +0300

    Lab8 started. Needs custom control and memento

---
+++ LAB8/CatalogPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LAB6_7
+{
+    /// <summary>
+    /// Interaction logic for CatalogPage.xaml
+    /// </summary>
+    public partial class CatalogPage : Page
+    {
+        public MainWindow mainWindow { get; }
+        public CatalogPage()
+        {
+            InitializeComponent();
+        }
+
+        public CatalogPage(MainWindow window)
+        {
+            mainWindow = window;
+            InitializeComponent();
+            GamesControl.ItemsSource = mainWindow.games;
+        }
+
+        private void ItemSample_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ObjectClasses.Game g = (ObjectClasses.Game)GamesControl.SelectedItem;
+            MessageBox.Show(g.FullDef);
+        }
+
+        private void delete_button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                var pet = button.DataContext as ObjectClasses.Game;
+                mainWindow.games.Remove(pet);
+
+            }
+            else { return; }
+        }
+    }
+}
